f2206d6dc1a85099ea13a4a23231e751d49a288f
Dantuluri Rohitha <dantulurirohitha@gmail.com>
2017-07-07 10:24:55 +0530

    Update careprovider.go

--- cp/careprovider.go
+++ cp/careprovider.go
@@ -104,21 +104,21 @@ func getPatient_byID(stub shim.ChaincodeStubInterface, args []string) (string, e
  queryArgs1 := make([][]byte, 2)
     queryArgs1[0] = []byte(f1)
      queryArgs1[1] = []byte(PatientID)
-  patientAsBytes, err := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
-  if err != nil {
-    errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+  pb := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
+  if pb.Message == "" {
+    errStr := fmt.Sprintf("Failed to query chaincode. Got error: ")
     fmt.Printf(errStr)
     return "", fmt.Errorf(errStr)
   }
   res := Patient{}
-  json.Unmarshal(patientAsBytes, &res)
+  json.Unmarshal([]byte(pb.Message), &res)
   fmt.Println(res)
   if res.PatientID == PatientID {
     fmt.Println("Patient found with PatientID : " + PatientID)
   } else {
     return "", fmt.Errorf("PatientID not found")
   }
-  return string(patientAsBytes),nil
+  return string(pb.Message),nil
 }
 
 func get_byCareProviderID(stub shim.ChaincodeStubInterface, args []string) (string, error) {
@@ -131,13 +131,13 @@ func get_byCareProviderID(stub shim.ChaincodeStubInterface, args []string) (stri
    queryArgs1 := make([][]byte, 2)
     queryArgs1[0] = []byte(f1)
      queryArgs1[1] = []byte(CareProviderID)
-  patientAsBytes, err := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
-  if err != nil {
-    errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+  pb := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
+  if pb.Message == "" {
+    errStr := fmt.Sprintf("Failed to query chaincode. Got error: ")
     fmt.Printf(errStr)
     return "", fmt.Errorf(errStr)
   }
-  return string(patientAsBytes),nil
+  return string(pb.Message),nil
 }
 
 func cupdate_patient(stub shim.ChaincodeStubInterface, args []string) (string, error) {
@@ -156,9 +156,9 @@ func cupdate_patient(stub shim.ChaincodeStubInterface, args []string) (string, e
      queryArgs1[2] = []byte(Medications)
      queryArgs1[3] = []byte(Remarks)
      queryArgs1[4] = []byte(User)
-  _, err := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
-  if err != nil {
-    errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+  pb := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
+  if pb.Message == "" {
+    errStr := fmt.Sprintf("Failed to query chaincode. Got error: ")
     fmt.Printf(errStr)
     return "", fmt.Errorf(errStr)
   }
@@ -179,9 +179,9 @@ func share_patient(stub shim.ChaincodeStubInterface, args []string) (string, err
     queryArgs1[0] = []byte(f1)
      queryArgs1[1] = []byte(PatientID)
      queryArgs1[2] = []byte(DoctorID)
-  _, err := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
-  if err != nil {
-    errStr := fmt.Sprintf("Failed to query chaincode. Got error: %s", err.Error())
+  pb := stub.InvokeChaincode(PatientChaincode, queryArgs1,"")
+  if pb.Message == "" {
+    errStr := fmt.Sprintf("Failed to query chaincode. Got error: ")
     fmt.Printf(errStr)
     return "", fmt.Errorf(errStr)
   }
